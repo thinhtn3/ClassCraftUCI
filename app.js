39dd6aa15908c86c197bad5eaa311d5eaa34100f
thinhtn3 <thinhttnguyen@gmail.com>
2023-12-20 21:03:17 -0800

    fixed table layout and also updated search

--- app.js
+++ app.js
@@ -8,49 +8,16 @@ let datalistOptions = document.querySelector('#departments');
 let displayCourses = document.querySelector('.displayCourses')
 const config = { headers: { Host: `` } }
 
-let datalistValid = []
+let datalistValid = ['I%26C+SCI',`CRM%2FLAW`]
 for (datalist of datalistOptions.options) {
     datalistValid.push(datalist.value)
 }
-
-let checkCourseCodeTrue = async (code) => {
-    try {
-        let classArr = []
-        let classInfo = {};
-        const res = await fetch(`https://api.peterportal.org/rest/v0/schedule/soc?term=${selectedTerm.value}&sectionCodes=${selectedCourseCode.value.trim()}`)
-        const data = await res.json();
-        classInfo.courseName = data.schools[0].departments[0].courses[0].deptCode;
-        classInfo.courseNumber = data.schools[0].departments[0].courses[0].courseNumber;
-        classInfo.courseTitle = data.schools[0].departments[0].courses[0].courseTitle;
-        classInfo.type = data.schools[0].departments[0].courses[0].sections[0].sectionType;
-        classInfo.section = data.schools[0].departments[0].courses[0].sections[0].sectionNum;
-        classInfo.units = data.schools[0].departments[0].courses[0].sections[0].units;
-        classInfo.instructor = data.schools[0].departments[0].courses[0].sections[0].instructors[0];
-        classInfo.days = data.schools[0].departments[0].courses[0].sections[0].meetings[0].days;
-        classInfo.time = data.schools[0].departments[0].courses[0].sections[0].meetings[0].time;
-        classInfo.building = data.schools[0].departments[0].courses[0].sections[0].meetings[0].bldg;
-        classInfo.currentEnrolled = data.schools[0].departments[0].courses[0].sections[0].numCurrentlyEnrolled.totalEnrolled;
-        classInfo.maxEnrolled = data.schools[0].departments[0].courses[0].sections[0].maxCapacity;
-        classInfo.currentWaitlist = data.schools[0].departments[0].courses[0].sections[0].numOnWaitlist;
-        classInfo.final = data.schools[0].departments[0].courses[0].sections[0].finalExam;
-        classInfo.restrictions = data.schools[0].departments[0].courses[0].sections[0].restrictions;
-        classInfo.status = data.schools[0].departments[0].courses[0].sections[0].status;
-        classArr.push(classInfo)
-        return classInfo;
-    } catch (e) {
-        console.log('There is an error', e)
-        alert(`Course code is invalid or does not exist, please try again`)
-    }
-}
-
 let checkDepartmentTrue = () => {
-    if (!selectedDepartment.value.trim()) {
-        alert('Please enter a department')
+    if (!selectedDepartment.value.trim().toUpperCase()) {
         return false
-    } else if (datalistValid.includes(selectedDepartment.value) === false) {
-        alert('Department does not exist')
+    } else if (datalistValid.includes(selectedDepartment.value.toUpperCase()) === false) {
         return false
-    } else {
+    } else if (datalistValid.includes(selectedDepartment.value.toUpperCase()) === true) {
         return true
     }
 }
@@ -63,20 +30,32 @@ let checkCourseNumberTrue = () => {
     }
 }
 
-let createTableHeaders = () => {
+let createTableHeaders = (name, num, title) => {
+    const nameNum = document.createElement('caption');
+    const span = document.createElement(`span`);
+    span.innerHTML = `${name} ${num}: `
+    span.classList.add(`bold`);
+    nameNum.append(span);
+    nameNum.append(title);
+
     const tr1 = document.createElement('tr');
     const code = document.createElement('th');
     code.rowSpan = 2;
     code.innerText = 'Code';
+    code.classList.add('smallTdTh')
+
     const type = document.createElement('th');
     type.rowSpan = 2;
     type.innerText = 'Type';
+    type.classList.add('smallTdTh')
     const section = document.createElement('th');
     section.rowSpan = 2;
     section.innerText = 'Section';
+    section.classList.add('smallTdTh')
     const unit = document.createElement('th');
     unit.rowSpan = 2;
     unit.innerText = 'Unit';
+    unit.classList.add('smallTdTh')
     const instructor = document.createElement('th');
     instructor.rowSpan = 2;
     instructor.innerText = 'Instructor';
@@ -85,6 +64,7 @@ let createTableHeaders = () => {
     time.innerText = 'Time';
     const bldg = document.createElement('th');
     bldg.rowSpan = 2;
+    bldg.classList.add('smallTdTh')
     bldg.innerText = 'Building';
     const final = document.createElement('th');
     final.innerText = 'Final';
@@ -94,12 +74,16 @@ let createTableHeaders = () => {
     enrollmentStatus.innerText = 'Enrollment';
     const enrollmentStatusHeader1 = document.createElement('th');
     enrollmentStatusHeader1.innerText = 'Enrolled';
+    enrollmentStatusHeader1.classList.add('smallTdTh')
     const enrollmentStatusHeader2 = document.createElement('th');
     enrollmentStatusHeader2.innerText = 'Max';
+    enrollmentStatusHeader2.classList.add('smallTdTh')
     const enrollmentStatusHeader3 = document.createElement('th');
     enrollmentStatusHeader3.innerText = 'Waitlist';
+    enrollmentStatusHeader3.classList.add('smallTdTh')
     const enrollmentStatusHeader4 = document.createElement('th');
     enrollmentStatusHeader4.innerText = 'Status';
+    enrollmentStatusHeader4.classList.add('smallTdTh')
     const tr2 = document.createElement('tr');
     tr2.append(enrollmentStatusHeader1);
     tr2.append(enrollmentStatusHeader2);
@@ -114,24 +98,32 @@ let createTableHeaders = () => {
     tr1.append(bldg);
     tr1.append(final);
     tr1.append(enrollmentStatus);
-    displayCourses.append(tr1);
-    displayCourses.append(tr2);
+    let newTable = document.createElement('div');
+    newTable.style.width = '100%'
+    newTable.style.textAlign = 'center'
+    newTable.append(nameNum)
+    newTable.append(tr1)
+    newTable.append(tr2)
+    return newTable;
 }
 
 let createCourse = (classes) => {
     let newTr = document.createElement('tr');
-
     courseCode = document.createElement('td');
     courseCode.innerText = classes.sectionCode;
+    courseCode.classList.add('smallTdTh')
     newTr.append(courseCode)
     courseType = document.createElement('td');
+    courseType.classList.add('smallTdTh')
     courseType.innerText = classes.sectionType;
     newTr.append(courseType)
     courseSection = document.createElement('td');
     courseSection.innerText = classes.sectionNum;
+    courseSection.classList.add('smallTdTh')
     newTr.append(courseSection)
     courseUnit = document.createElement('td');
     courseUnit.innerText = classes.units;
+    courseUnit.classList.add('smallTdTh')
     newTr.append(courseUnit)
     courseInstructor = document.createElement('td');
     courseInstructor.innerText = classes.instructors;
@@ -141,61 +133,111 @@ let createCourse = (classes) => {
     newTr.append(courseTime)
     courseBuilding = document.createElement('td');
     courseBuilding.innerText = classes.meetings[0].bldg;
+    courseBuilding.classList.add('smallTdTh')
     newTr.append(courseBuilding)
     courseFinal = document.createElement('td');
     courseFinal.innerText = classes.finalExam;
-    newTr.append(courseFinal)
+    newTr.append(courseFinal);
     courseCurrentEnrolled = document.createElement('td');
     courseCurrentEnrolled.innerText = classes.numCurrentlyEnrolled.totalEnrolled;
-    newTr.append(courseCurrentEnrolled)
+    courseCurrentEnrolled.classList.add('smallTdTh');
+    newTr.append(courseCurrentEnrolled);
     courseMaxCapacity = document.createElement('td');
     courseMaxCapacity.innerText = classes.maxCapacity;
+    courseMaxCapacity.classList.add('smallTdTh');
     newTr.append(courseMaxCapacity)
     courseCurrentWaitlist = document.createElement('td');
     courseCurrentWaitlist.innerText = classes.numOnWaitlist;
+    courseCurrentWaitlist.classList.add('smallTdTh');
     newTr.append(courseCurrentWaitlist)
     courseStatus = document.createElement('td');
     courseStatus.innerText = classes.status;
+    courseStatus.classList.add('smallTdTh')
     newTr.append(courseStatus)
+    return newTr;
+}
 
-    displayCourses.append(newTr)
+let appendCourse = (data) => {
+    for (number of data.schools[0].departments[0].courses) {
+        let courseDiv = document.createElement('div');
+        courseDiv.append(createTableHeaders(number.deptCode, number.courseNumber, number.courseTitle));
+        for (course of number.sections) {
+            courseDiv.append(createCourse(course));
+        }
+        console.log(courseDiv)
+        displayCourses.append(courseDiv);
+    }
+}
+
+let appendCourseGe = (data) => {
+    for (school of data.schools) {
+        for (department of school.departments) {
+            for (number of department.courses) {
+                let courseDiv = document.createElement('div');
+                courseDiv.append(createTableHeaders(number.deptCode, number.courseNumber, number.courseTitle));
+                for (course of number.sections) {
+                    courseDiv.append(createCourse(course));
+                }
+                console.log(courseDiv)
+                displayCourses.append(courseDiv);
+            }
+        }
+    }
 }
 
 submitForm.addEventListener('click', async (e) => {
     e.preventDefault();
-    let departmentValid = checkDepartmentTrue();
-    displayCourses.innerHTML= ''
+    displayCourses.innerHTML = '';
+
+    if (selectedDepartment.value === 'I&C SCI') {
+        selectedDepartment.value = 'I%26C+SCI';
+    } else if (selectedDepartment.value === 'CRM/LAW') {
+        selectedDepartment.value = 'CRM%2FLAW';
+    }
+
+    let departmentTrue = checkDepartmentTrue();
 
     if (selectedCourseCode.value.trim() !== '') {
-        let classArr = [];
-        let res = await checkCourseCodeTrue(selectedCourseCode.value.trim());
-        classArr.push(res);
-        createTableHeaders();
-        for (classes of classArr) {
-            createCourse(classes, classArr)
+        let res = await fetch(`https://api.peterportal.org/rest/v0/schedule/soc?sectionCodes=${selectedCourseCode.value.trim()}&term=${selectedTerm.value}`)
+        let data = await res.json();
+        appendCourse(data)
+
+    } else if (selectedGe.value.trim() !== '' && selectedGe.value.trim() !== 'ANY') {
+        if (departmentTrue === false) {
+            try {
+                let res = await fetch(`https://api.peterportal.org/rest/v0/schedule/soc?ge=${selectedGe.value.trim()}&term=${selectedTerm.value}`)
+                let data = await res.json();
+                appendCourseGe(data);
+            } catch (e) {
+                console.log(e)
+            }
+        } else if (departmentTrue === true) {
+            try {
+                let res = await fetch(`https://api.peterportal.org/rest/v0/schedule/soc?ge=${selectedGe.value}&term=${selectedTerm.value}&department=${selectedDepartment.value.trim()}`)
+                let data = await res.json();
+                appendCourseGe(data);
+            } catch (e) {
+                console.log(e)
+            }
+        }
+    } else if (departmentTrue === true && selectedCourseNumber.value.trim() === '') {
+        try {
+            let res = await fetch(`https://api.peterportal.org/rest/v0/schedule/soc?department=${selectedDepartment.value.trim()}&term=${selectedTerm.value}`);
+            let data = await res.json();
+            appendCourse(data)
+        } catch (e) {
+            console.log(e);
+            alert(`Department does not exist. Try again.`)
         }
 
-    } else if (departmentValid === true && selectedCourseNumber.value.trim() === '') {
+    } else if (departmentTrue === true && selectedCourseNumber.value.trim() !== '') {
         try {
-            let res = await fetch(`https://api.peterportal.org/rest/v0/schedule/soc?department=${selectedDepartment.value.trim()}&term=${selectedTerm.value}`)
+            let res = await fetch(`https://api.peterportal.org/rest/v0/schedule/soc?department=${selectedDepartment.value.trim()}&courseNumber=${selectedCourseNumber.value.trim()}&term=${selectedTerm.value}`);
             let data = await res.json();
-            for (number of data.schools[0].departments[0].courses) {
-                createTableHeaders();
-                for (course of number.sections) {
-                    createCourse(course)
-                }
-            }
+            appendCourse(data)
         } catch (e) {
-            console.log(e)
-        }
-    } else if (departmentValid === true && selectedCourseNumber.value.trim() !== '') {
-        let res = await fetch(`https://api.peterportal.org/rest/v0/schedule/soc?department=${selectedDepartment.value.trim()}&courseNumber=${selectedCourseNumber.value.trim()}&term=${selectedTerm.value}`);
-        let data = await res.json();
-        for (number of data.schools[0].departments[0].courses) {
-            createTableHeaders();
-            for (course of number.sections) {
-                createCourse(course)
-            }
+            console.log(e);
+            alert(`Course either not offered or does not exist. Try again.`)
         }
     }
 });
